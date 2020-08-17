55123bbbe0de1752b8d4639fd4377b7030bfdf62
David Stein <davidmfstein@gmail.com>
2020-08-17 15:24:02 -0400

    hack orientation detection

--- dash_app/apps/dash-singlecell-vr/static/loading-component.js
+++ dash_app/apps/dash-singlecell-vr/static/loading-component.js
@@ -28,16 +28,15 @@ AFRAME.registerComponent('loading', {
         const width = this.visibleWidthAtZDepth(this.data.zDepth);
         const height = this.visibleHeightAtZDepth(this.data.zDepth);
         const matchMedia = window.webkitMatchMedia || window.mozMatchMedia || window.oMatchMedia || window.msMatchMedia || window.matchMedia;
-        if (matchMedia("(orientation: portrait)").matches || 
-            ["portrait", "portrait-primary", "portrait-secondary"].includes(window.screen.orientation) ||
-            (Utils.mobileCheck() && window.height > window.width)) {
+        const isPortrait = (matchMedia && matchMedia("(orientation: portrait)").matches) || 
+                            (["portrait", "portrait-primary", "portrait-secondary"].includes(window.screen.orientation) ||
+                            (Utils.mobileCheck() && window.height > window.width))
+        if (isPortrait) {
             this.el.object3D.rotation.set(0, 0, -32.987);
             this.el.setAttribute('width', height);
             this.el.setAttribute('height', width);
             document.getElementById('hud').object3D.position.set(-width/2 + .25, height/2 - .25, this.data.zDepth);
-        } else if (matchMedia("(orientation: landscape)").matches ||
-            ["landscape", "landscape-primary", "landscape-secondary"].includes(window.screen.orientation) ||
-            (Utils.mobileCheck() && window.height > window.width)) {
+        } else {
             this.el.object3D.rotation.set(0, 0, 0);
             this.el.setAttribute('width', width);
             this.el.setAttribute('height', height);
