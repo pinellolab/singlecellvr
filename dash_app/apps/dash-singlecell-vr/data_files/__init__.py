6dfb209783cb4c73d0e6925d567b200d784fa855
mvinyard <mvinyard@broadinstitute.org>
2020-12-11 00:59:38 +0000

    data loader from figshare

---
+++ dash_app/apps/dash-singlecell-vr/data_files/__init__.py
@@ -0,0 +1,10 @@
+# data files __init__.py
+
+__author__ = ', '.join([
+    'Michael E. Vinyard'
+])
+__email__ = ', '.join([
+    'vinyard@g.harvard.edu',
+])
+
+from .load_figshare import load_from_figshare
